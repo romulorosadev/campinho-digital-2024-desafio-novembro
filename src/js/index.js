3c34f2dd5270377c3ea9fdc749bcd20bcc442e23
Romulo Rosa <romulorosadev@gmail.com>
2024-11-06 18:28:12 -0300

    js criar funcao configurarcabecalhos

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,11 @@ const apiUrl = 'https://ecom-back-strapi.onrender.com/api/products';
 
 //2 - Criar variavel para armazenar/manipular o toke/chave de acesso para api
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzMwOTI2OTYzLCJleHAiOjE3MzM1MTg5NjN9.5oZdqaREzoAKCTKiXINGqQj32g50S0KoXmSKAf5FJhI';
+
+//3 - Criar função que retorna os cabeçalhos pra requisição
+function configurarCabecalhos() {
+  return {
+    'Authorization': token,
+    'Content-Type': 'application/json'
+  };
+}
