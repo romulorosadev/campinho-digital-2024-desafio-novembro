d53971497ae6d1cd3f352706438561bb5ebe6272
Romulo Rosa <romulorosadev@gmail.com>
2024-11-06 18:25:31 -0300

    js criar variavel pra token da api

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
-//1- Definir endpoint/url da api
+//1- Criar variavel para armazenar/manipular o endpoint/url da api
 const apiUrl = 'https://ecom-back-strapi.onrender.com/api/products';
+
+//2 - Criar variavel para armazenar/manipular o toke/chave de acesso para api
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzMwOTI2OTYzLCJleHAiOjE3MzM1MTg5NjN9.5oZdqaREzoAKCTKiXINGqQj32g50S0KoXmSKAf5FJhI';
